0c68e69eb03e7a75516b30b5328ca4a51ef76de6
julife12 <julian.jaimes.2019@upb.edu.cp>
2019-08-17 19:04:18 -0500

    mi primera modificacion

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 a=int(input("ingrese el primer numero: "))
-b=int(input("ingrese el segundo numero: "))
-total=a*b
+c=int(input("ingrese el segundo numero: "))
+b=int(input("ingrese el numero para encontrar cuadrado: "))
+d=int(input("ingrese el numero para encontrar la raiz cuadrada: "))
+total=a*c
 print("el resultado es: ", total)
 doble=a*2
 print("el resultado es: ", doble)
+cuadrado=b*b
+print("es resulado del cuadrado es: ", cuadrado)
+raiz=d**.5
+print("el resultado de", raiz)
+
