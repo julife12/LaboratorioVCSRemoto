d4c57d3f30d33ff360cda3329590a6249bcca912
julife12 <julian.jaimes.2019@upb.edu.cp>
2019-08-18 13:59:12 -0500

    correccion solucion1

--- main.py
+++ main.py
@@ -2,16 +2,20 @@ print("hola bienvenido a el software para solucionar ecuaciones de tipo, ax2+bx+
 a=int(input("ingrese el numero del primer termino: "))
 b=int(input("ingrese el numero del segundo termino: "))
 c=int(input("ingrese el numero del tercer termino: "))
-d=((b*b)-(4*a*c))
-doble=a*2
-if d<0:
-    x1=(-(b)+(d**0.5))/doble
-    x2=(-(b)-(d**0.5))/doble
+cuadrado=b*b
+ac4=a*c*4
+d= (cuadrado-ac4)
+if d>0:
+    import math
+    x1=(((-b)+(math.sqrt(d))/a*2))
+    x2=(((-b)-(math.sqrt(d))/a*2))
     print("la solucion para x1= ", x1, "la solucion para x2= ", x2)
-elif d==0:
-    x=(-(b))/doble
-    print("la solucion es x= ", x)
 else:
-    print("no existe solucion en los numeros")
+    if d==0:
+        
+        x=(-(b))/a*2
+        print("la solucion es x= ", x)
+    else:
+        print("no existe solucion en los numeros")
 
 
