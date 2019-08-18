0d0dde64af1c93fe5e5d181d0698b201d7fec95f
julife12 <julian.jaimes.2019@upb.edu.cp>
2019-08-17 22:44:04 -0500

    agregamos la expresion que nos dara paso a saber que procedimiento hay que seguir

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 a=int(input("ingrese el numero del primer termino: "))
 b=int(input("ingrese el numero del segundo termino: "))
 c=int(input("ingrese el numero del tercer termino: "))
-total=a*c
+d=((b*b)-(4*a*c))
+
 print("el resultado es: ", total)
 doble=a*2
 print("el resultado es: ", doble)
-cuadrado=b*b
-print("es resulado del cuadrado es: ", cuadrado)
-raiz=d**.5
-print("el resultado de", raiz)
-
