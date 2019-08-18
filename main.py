62984dbeaaed851591b4dbaa3101ed8e63905dc0
julife12 <julian.jaimes.2019@upb.edu.cp>
2019-08-17 22:54:25 -0500

    agregamos la ecuasion para solucionar si d<0

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ a=int(input("ingrese el numero del primer termino: "))
 b=int(input("ingrese el numero del segundo termino: "))
 c=int(input("ingrese el numero del tercer termino: "))
 d=((b*b)-(4*a*c))
-
-print("el resultado es: ", total)
 doble=a*2
-print("el resultado es: ", doble)
+if d<0:
+    x1=(-(b)+(d**0.5))/doble
+    x2=(-(b)-(d**0.5))/doble
+    print("la solucion para x1= ", x1, "la solucion para x2= ", x2)
