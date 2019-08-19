635e6d0ac789f8da1a98fecc31e30d3cc60901f8
julife12 <julian.jaimes.2019@upb.edu.cp>
2019-08-18 22:30:24 -0500

    la ecuacion no estaba bien planteada entonces me toco separar por partes para que el sistema la leyera de una manera adecuada y diera un resultado verdadero

--- main.py
+++ main.py
@@ -2,18 +2,21 @@ print("hola bienvenido a el software para solucionar ecuaciones de tipo, ax2+bx+
 a=int(input("ingrese el numero del primer termino: "))
 b=int(input("ingrese el numero del segundo termino: "))
 c=int(input("ingrese el numero del tercer termino: "))
-cuadrado=b*b
-ac4=a*c*4
-d= (cuadrado-ac4)
+cuadrado= b*b
+multiplicacion=a*c
+d= cuadrado-4*multiplicacion
+doble=2*a
 if d>0:
     import math
-    x1=(((-b)+(math.sqrt(d))/a*2))
-    x2=(((-b)-(math.sqrt(d))/a*2))
+    x1=((-b)+(math.sqrt(d)))
+    x1_1=x1/doble
+    x2=((-b)-(math.sqrt(d)))
+    x2_1=x2/doble
     print("la solucion para x1= ", x1, "la solucion para x2= ", x2)
 else:
     if d==0:
         
-        x=(-(b))/a*2
+        x=(-b)/doble
         print("la solucion es x= ", x)
     else:
         print("no existe solucion en los numeros")
