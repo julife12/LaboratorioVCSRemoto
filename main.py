d0d16a5331077a14f03e4084e72a2815561b7a10
julife12 <julian.jaimes.2019@upb.edu.cp>
2019-08-19 13:35:42 -0500

    a la hora de mostrar la solucion modifique la variable porque etaba dando un resultado malo

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ b=int(input("ingrese el termino b: "))
 c=int(input("ingrese el termino c: "))
 cuadrado= b*b
 multiplicacion=a*c
-d= cuadrado-4*multiplicacion
+import math
+d=math.sqrt(cuadrado-4*multiplicacion)
 doble=2*a
 if d>0:
-    import math
-    x1=((-b)+(math.sqrt(d)))
+    x1=((-b)+d)
     x1_1=x1/doble
-    x2=((-b)-(math.sqrt(d)))
+    x2=((-b)-(d))
     x2_1=x2/doble
-    print("la solucion para x1= ", x1, "la solucion para x2= ", x2)
+    print("la solucion para x1= ", x1_1, "la solucion para x2= ", x2_1)
 else:
     if d==0:
         
